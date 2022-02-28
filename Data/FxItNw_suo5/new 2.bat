e0364362fdfbb789d9b1958da56100cbb0d4825d
Mateo Buteler <mateojosebuteler@gmail.com>
2022-02-28 16:24:55 -0300

    remove FixItNow (it's useless)

--- Data/FxItNw_suo5/new 2.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set VAR=antes
-    if "%VAR%" == "antes" (
-        set VAR=después
-        if "%VAR%" == "después" echo Si esto se puede ver, entonces
-                         significa que funcionó
-    )
-	pause
